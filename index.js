1f4b0d86a3a9fd0fbe936c626e7e23b3ad778399
Uzor Ogechukwu <ogemama903@gmail.com>
2023-03-02 14:15:41 -0800

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-console.log('uzor ogechukwu');
+//console.log('uzor ogechukwu');
 
 //functions that multiply two numbers//
-function multiply (a,b) { return a * b }
+function multiply (a,b) { return a * b };
 
-console. log (multiply(49,5))
+console. log (multiply(49,5));
+
+//my first name is uzor
+// my last name is ogechukwu
+// my stutren track is front end 
+// skilled with HTML and CSS
+// my favourite color is blue
+//country of independence is Nigeria
+
+let first_name = 'uzor';
+let last_name = 'ogechukwu';
+let stutren_track = 'frontend engineering';
+let skill = 'html and css';
+let color = 'blue';
+let country_of_independence = 'Nigeria';
+
+
+console.log(first_name);
+console.log(last_name);
+console.log(stutren_track);
+console.log(skill);
+console.log(color);
+console.log(country_of_independence);
+
+console.log('My name is ' + first_name + ' ' +last_name + '. I am a ' + stutren_track + ', am skilled with ' + skill + '. my favourite color is ' + color + '. My country of independence is  ' + country_of_independence + '.');
+
